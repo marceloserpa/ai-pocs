d22022b212ecbedf533aeb6de6541b7fa3a987d0
marceloserpa <jdev.marcelo@gmail.com>
2026-05-23 14:00:19 -0300

    deberta-v3 tokenizer

---
+++ tokens/tokenizer-decode.py
@@ -0,0 +1,14 @@
+from transformers import AutoModel, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+model = AutoModel.from_pretrained("microsoft/deberta-v3-xsmall")
+
+tokens = tokenizer('Hello world', return_tensors='pt')
+
+output = model(**tokens)[0]
+
+print(output.shape)
+
+
+for token in tokens['input_ids'][0]:
+    print(tokenizer.decode(token))
