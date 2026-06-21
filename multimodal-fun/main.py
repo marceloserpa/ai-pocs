b8253bf725047ba446572480310fecadbf35a86b
marceloserpa <jdev.marcelo@gmail.com>
2026-06-21 11:46:19 -0300

    multimodal - CLIP

--- multimodal-fun/main.py
+++ multimodal-fun/main.py
@@ -1,3 +1,7 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
 from urllib.request import urlopen
 from PIL import Image
 
@@ -15,5 +19,31 @@ model = CLIPModel.from_pretrained(model_id)
 
 inputs = clip_tokenizer(caption, return_tensors="pt")
 print(inputs)
-
 print(clip_tokenizer.convert_ids_to_tokens(inputs["input_ids"][0]))
+
+text_embedding = model.get_text_features(**inputs)
+processed_image = clip_processor(
+    text=None, images=image, return_tensors="pt"
+)["pixel_values"]
+
+# Prepare image for visualization
+img = processed_image.squeeze(0)
+img = img.permute(*torch.arange(img.ndim - 1, -1, -1))
+img = np.einsum("ijk->jik", img)
+
+# Visualize preprocessed image
+plt.imshow(img)
+plt.axis("off")
+
+image_embedding = model.get_image_features(processed_image)
+
+# Normalize the embeddings
+text_embedding /= text_embedding.norm(dim=-1, keepdim=True)
+image_embedding /= image_embedding.norm(dim=-1, keepdim=True)
+
+
+# Calculate their similarity
+text_embedding = text_embedding.detach().cpu().numpy()
+image_embedding = image_embedding.detach().cpu().numpy()
+score = np.dot(text_embedding, image_embedding.T)
+print(score)
